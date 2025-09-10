e138a40219f89c02a477361d20a922e0cd3d2beb
8-Bit Michael <mowz2012@gmail.com>
2025-09-09 21:12:11 -0400

    Updated: memory_manager.py
    I updated the allocation function to include the ability to traverse the list and find empty memory blocks, which were actually nodes, of the requested size or to split an existing one so however much size you wanted would be taken from it and put into the new block, and the other gets that size subtracted from it. This was done using linked lists, loops, and iteration.

--- memory_manager.py
+++ memory_manager.py
@@ -14,14 +14,28 @@ class MemoryManager:
     
     def initialize_memory(self, total_size):
         start = 0
-        size = total_size
+        size = total_size # Size = The size of the new block.
         is_free = True
         self.head = MemoryBlock(start, size, is_free, None, self) # The head of the linked list representing memory blocks.
 
 
-    def allocate(self, size):
-        pass
+    def allocate(self, size): # Size = The size of code set aside for something.
+        current = self.head
+        while current: # Make this 'traverse' somehow.
+            if current.next == True: # If there is a node/memory block pointer.
+                current = current.next # Switch from the head to the next block? Could be assigning the next as the head.
+                if self.current == True and self.size == size: # If there is a new current and it's equal to the requested size.
+                    self.current = size # Make the current node become the requested block size.
+                else:
+                    continue # Continue going through.
+            else:
+                return
 
+        if self.size > size:
+            is_free = True
+            start = 0 # Start off with a value of 0? But zero is still a value?
+            head_1 = MemoryBlock(start, (size), is_free, None, self)
+            head_2 = MemoryBlock(start, (self.size - size), is_free, None, self)
     def free(self, address):
         pass
 
